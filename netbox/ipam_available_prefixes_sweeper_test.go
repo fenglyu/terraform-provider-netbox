c59af80e054e4bee44865ae05b9359c1ea8bea0a
Feng Lyu <flv@blizzard.com>
2020-08-10 12:50:54 +0800

    draft a global testing sweeper

---
+++ netbox/ipam_available_prefixes_sweeper_test.go
@@ -0,0 +1,17 @@
+package netbox
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
+
+// sharedClientForRegion returns a common provider client configured for the specified region
+func sharedClientForRegion(region string) (interface{}, error) {
+
+	return nil, nil
+}
